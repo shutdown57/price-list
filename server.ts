d20ec6227bd2d64c879abd9d9290c32b047d24d6
Majid Mohamadi <m.shutdown57@gmail.com>
2022-10-07 20:41:43 +0330

    Added routes

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 // import 'regenerator-runtime/runtime'
 
-import { Express } from 'express'
+import { Application } from 'express'
 import express from 'express'
 import { Server } from 'http'
 import http from 'http'
@@ -10,9 +10,8 @@ import { v4 as uuid4 } from 'uuid'
 
 import { database } from './models'
 import VendorRouter from './routes/vendor.route'
-// import MainRoute from './routes/main.route'
-// import UserIO from './io/user.io'
-// import ChatIO from './io/chat.io'
+import PriceListRouter from './routes/priceList.route'
+import PriceListDetailRouter from './routes/priceListDetail.route'
 
 
 // Read env file
@@ -21,14 +20,16 @@ dotenv.config()
 try {
   (async () => {
     await database.authenticate()
-    await database.sync()
+    await database.sync({ alter: { drop: true } })
   })()
   console.log('Connection has been established successfully.')
 } catch (error) {
   console.error('Unable to connect to the database:', error)
 }
 
-const app: Express = express()
+const app: Application = express()
+
+app.use(express.json())
 const server: Server = http.createServer(app)
 const io = new IOServer(server, {
   cors: {
@@ -40,11 +41,10 @@ io.engine.generateId = () => {
   return uuid4()
 }
 
-// UserIO(io)
-// ChatIO(io)
-
 // Routes
 app.use('/vendor', VendorRouter)
+app.use('/price-list', PriceListRouter)
+app.use('/price-list-detail', PriceListDetailRouter)
 
 const port: number | undefined = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000
 
