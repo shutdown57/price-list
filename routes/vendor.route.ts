4a4d8809bdc6e713abc298f7c00694c49297796e
Majid Mohamadi <m.shutdown57@gmail.com>
2022-10-07 20:40:09 +0330

    Fixed vendor route file

--- routes/vendor.route.ts
+++ routes/vendor.route.ts
@@ -1,21 +1,53 @@
-// import { Rule } from '../models/rule.model'
-import { Response, Router } from 'express'
+import { Request, Response, Router } from 'express'
 
-// import RuleService from '../services/rule.service'
-// import { User } from '../models/user.model'
-// import VendorService from '../services/vendor.service'
+import VendorService from '../services/vendor.service'
+import { BodyCreate, BodyUpdate, ParamId } from '../interfaces/RequestVendor'
+import { QueryPaginate } from '../interfaces/RequestCommon'
 
 
 const router: Router = Router()
 
-router.get('/', async (_, res: Response): Promise<any> => {
-  // const vendorService = new VendorService()
-  // console.log(vendorService)
-  // const ruleService = new RuleService()
-  // const rules = await ruleService.all()
-  // const users = await User.findAll({ include: Rule })
-  // const user = await User.findOne({ where: { id: '27a297f3-8135-4784-b666-2311adabef52' } })
-  res.json({ message: 'OK' })
+router.get('/', async (req: Request<unknown, unknown, unknown, QueryPaginate>, res: Response) => {
+  const { query } = req
+  const page = query.page ?? 1
+  const limit = query.limit ?? 20
+  const offset = (page - 1) * limit
+  const vendorService = new VendorService()
+  const data = await vendorService.paginated(limit, offset)
+  res.json({ ...data })
+})
+
+router.post('/', async (req: Request<BodyCreate>, res: Response): Promise<any> => {
+  const vendorService = new VendorService()
+  const { name, countries } = req.body
+  const vendor = await vendorService.create(name, countries)
+  res.json({ ...vendor.toJSON() })
+})
+
+router.get('/:vendorId', async (req: Request<ParamId>, res: Response): Promise<any> => {
+  const vendorService = new VendorService()
+  const vendor = await vendorService.byId(req.params.vendorId)
+  if (!vendor) {
+    res.status(404)
+  }
+  res.json({ ...vendor?.toJSON() })
+})
+
+router.put('/:vendorId', async (req: Request<ParamId, BodyUpdate>, res: Response): Promise<any> => {
+  const vendorService = new VendorService()
+  const { vendorId } = req.params
+  const { name, countries } = req.body
+  await vendorService.update(vendorId, name, countries)
+  res.status(204)
+  res.json({})
+})
+
+router.delete('/:vendorId', async (req: Request<ParamId>, res: Response): Promise<any> => {
+  const vendorService = new VendorService()
+  const { vendorId } = req.params
+  await vendorService.delete(vendorId)
+  res.status(204)
+  res.json({})
 })
 
 export default router
